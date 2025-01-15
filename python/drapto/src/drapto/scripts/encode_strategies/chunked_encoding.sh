35a2c22c9ca97fb317579ddc8c9944d8547bc970
five82 <five82@users.noreply.github.com>
2025-01-15 23:03:10 +0000

    feat: create encoding strategy files
    - Create chunked_encoding.sh for chunked encoding strategy
    - Create dolby_vision.sh for Dolby Vision strategy
    - No code moves yet, just initial file setup

---
+++ python/drapto/src/drapto/scripts/encode_strategies/chunked_encoding.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+###################
+# Chunked Encoding Strategy
+###################
+
+source "${SCRIPT_DIR}/utils/formatting.sh"
---
+++ python/drapto/src/drapto/scripts/encode_strategies/dolby_vision.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+###################
+# Dolby Vision Strategy
+###################
+
+source "${SCRIPT_DIR}/utils/formatting.sh"
